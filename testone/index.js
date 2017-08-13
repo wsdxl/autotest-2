4ff173b90389de9480c0a152056ce991c70071e5
wsdxl <dxl20@163.com>
2017-08-13 11:08:48 +0800

    20170813

--- testone/index.js
+++ testone/index.js
@@ -1,8 +1,8 @@
 require('chromedriver');
-var webdriver=require('selenium-webdriver');
-var By=webdriver.By;
-let driver=new webdriver.Builder().forBrowser('chrome').build();
+var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+let driver = new webdriver.Builder().forBrowser('chrome').build();
 driver.get('https://www.baidu.com/');
 driver.manage().window().maximize();
 driver.findElement(By.id('kw')).sendKeys('testing');
-driver.findElement({className:'btn self-btn bg s_btn'}).click();
+driver.findElement({ id: 'su' }).click();

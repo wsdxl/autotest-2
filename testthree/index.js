31d3a0c33fca48ee910b3ec8deec389ea7335551
wsdxl <dxl20@163.com>
2017-08-20 17:31:16 +0800

    20170820

---
+++ testthree/index.js
@@ -0,0 +1,48 @@
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder().forBrowser('chrome').build();
+// driver.get('https://www.baidu.com/');
+// driver.manage().window().maximize();
+// var gengduo=driver.findElement({name:'tj_briicon'});
+// driver.actions().mouseMove(gengduo).perform();
+// driver.sleep(3000);
+// driver.findElement({name:'tj_mp3'}).click();
+// driver.getAllWindowHandles().then((win)=>{
+//     console.log(win.length)
+//     driver.switchTo().window(win[0]);
+// driver.close();
+// })
+// driver.get('https://www.baidu.com/');
+// driver.manage().window().maximize();
+// driver.findElement({ id: 'kw' }).sendKeys('hello');
+// driver.findElement({ id: 'kw' }).submit();
+// driver.sleep(5000);
+// driver.findElement({ xpath: '//*[@id="2"]/h3/a' }).click();
+// driver.getAllWindowHandles().then((win) => {
+//     driver.switchTo().window(win[0]);
+//     driver.close();
+//     driver.switchTo().window(win[1]);
+//     driver.sleep(3000);
+// })
+// driver.takeScreenshot().then((img) => {
+//     require('fs').writeFileSync('01.png', img, 'base64');
+// })
+// driver.sleep(3000);
+// driver.navigate().back();
+// driver.sleep(3000);
+// driver.navigate().forward();
+// driver.sleep(3000);
+// driver.navigate().refresh();
+// var path=require('path');
+// driver.get('https://www.baidu.com');
+// var pngpath=path.join(__dirname,'/01.png');
+// driver.findElement({css:'.soutu-btn'}).click();
+// driver.findElement({css:'.upload-pic'}).sendKeys(pngpath);
+
+
+driver.get('https://www.baidu.com');
+var path=require('path');
+var dirpath=path.join(__dirname,'image');
+driver.takeScreenshot().then((imp)=>{
+    require('fs').writeFileSync(dirpath+'/01.png',imp,'base64') // 截屏到指定文件夹
+})
---
+++ testthree/test.js
@@ -0,0 +1,4 @@
+const n = Math.random(1)*10
+console.log(n)
+const num = Math.round(n)
+console.log(num);
